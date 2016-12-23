3b537a8a68af6a091e28ae173319af21b24eecb1
kesac <kevin@turtlesort.com>
2016-12-23 00:01:20 -0700

    Process events now go to the console

--- Bremmer/Bremmer.CommandLine/Start.cs
+++ Bremmer/Bremmer.CommandLine/Start.cs
@@ -14,22 +14,29 @@ namespace Bremmer.CommandLine
             if(args.Length > 0)
             {
                 string source = args[0];
-
                 try
                 {
                     SiteBuilder builder = new SiteBuilder(source);
+                    builder.Processed += Builder_Processed;
 
                     System.Console.WriteLine("Site build started...");
                     builder.Build();
                     System.Console.WriteLine("Site build completed: " + builder.Target.FullName);
+                    System.Console.ReadKey();
 
                 }
                 catch (Exception e)
                 {
                     System.Console.WriteLine(e.Message);
+                    System.Console.ReadKey();
                 }
-                
+
             }
+        }
+
+        private static void Builder_Processed(object sender, EventArgs e)
+        {
+            System.Console.WriteLine(((ProcessEventArgs)e).Description);
         }
     }
 }
