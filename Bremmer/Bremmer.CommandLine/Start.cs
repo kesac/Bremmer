e02996c977ba51de8a0af41dedda32c7a3a0ea6e
kesac <kevin@turtlesort.com>
2016-12-29 13:39:30 -0700

    Bremmer.CommandLine now runs in its own AppDomain

--- Bremmer/Bremmer.CommandLine/Start.cs
+++ Bremmer/Bremmer.CommandLine/Start.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bremmer;
+using System.Security.Policy;
+using System.Security;
+using System.Reflection;
+using System.Security.Permissions;
 
 namespace Bremmer.CommandLine
 {
@@ -11,17 +15,20 @@ namespace Bremmer.CommandLine
     {
         public static void Main(string[] args)
         {
-            if(args.Length > 0)
+            if (AppDomain.CurrentDomain.IsDefaultAppDomain())
+            {
+                SetupAppDomain(args);
+                return;
+            }
+
+            if (args.Length > 0)
             {
                 string source = args[0];
                 try
                 {
                     SiteBuilder builder = new SiteBuilder(source);
-                    builder.Processed += Builder_Processed;
-
-                    System.Console.WriteLine("Site build started...");
+                    builder.Processed += HandleProcessEvent;
                     builder.Build();
-
                 }
                 catch (Exception e)
                 {
@@ -29,11 +36,37 @@ namespace Bremmer.CommandLine
                 }
 
             }
+            else
+            {
+                System.Console.Write("Provide the build directory path as the first argument");
+            }
         }
 
-        private static void Builder_Processed(object sender, EventArgs e)
+        private static void HandleProcessEvent(object sender, EventArgs e)
         {
             System.Console.WriteLine(((ProcessEventArgs)e).Description);
+        }
+
+        private static void SetupAppDomain(string[] args)
+        {
+            // For temporary files (https://github.com/Antaris/RazorEngine#temporary-files)
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+
+            AppDomainSetup ads = new AppDomainSetup();
+            ads.ApplicationBase = currentDomain.SetupInformation.ApplicationBase;
+            StrongName[] trustedAssemblies = new StrongName[0];
+
+            AppDomain domain = AppDomain.CreateDomain(
+                "Bremmer", 
+                null,
+                currentDomain.SetupInformation, 
+                new PermissionSet(PermissionState.Unrestricted),
+                trustedAssemblies
+            );
+
+            domain.ExecuteAssembly(Assembly.GetExecutingAssembly().Location, args);
+            AppDomain.Unload(domain);
         }
     }
 }
